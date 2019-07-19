4205e899d993f1cc89f51173d9b7cfa77b9336f4
DILLON XIE (ITA-ISDC-ISD-OOCLL/ZHA) <XIEDI@OOCL.COM>
2019-07-19 14:37:12 +0800

    finish first Counter

---
+++ src/components/test01.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="example-1">
+        <button id="add" @click="add">+</button>
+        <span>{{count}}</span>
+        <button id="sub" @click="sub">-</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "test01",
+        el: '#example-1',
+        data () {
+            return {
+                count: 0,
+            }
+        },
+        methods: {
+            add: function () {
+                count++;
+            },
+            sub: function () {
+                count--;
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+
+</style>
